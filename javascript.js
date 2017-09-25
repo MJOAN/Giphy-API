bd74ecda5f0b14b7becd1bceebe7e0ac70653ec9
Mariam Joan <royalton@gmail.com>
2017-09-24 20:42:59 -0700

    09-24

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-var caption, state, data, results, giphDiv;
+var caption, img, state, data, results;
 var topics = ["engineering", "electrons", "photons", "Ohm's Law", "sensor technology", "engines", "CPU", "transistors", "circuits", "information theory", "E=MC^2", "CPU", "neutrino", "quark"];
 var APIkey = "WrXZbvLFBbcOdunSWBG3md89agFdJE5y";
 var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=" + APIkey + "&limit=10");
@@ -6,58 +6,61 @@ var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=" 
 
 function generateButtons() {
 
-    $("#engineer-view").empty();
+    $("#engineering-view").empty();
 
     for (var i = 0; i < topics.length; i++) {
+
         var button = $("<button>");
 
         button.addClass("btn");
         button.css("margin", "3px");
         button.attr("data-name", topics[i]);
         button.text(topics[i]);
-        $("#engineer-view").append(button).prepend($("form"));
-    }
 
-    $(".btn").on("click", function(event) {
+        $("#engineering-view").append(button);
+    }
+}
+
+    $("button").on("click", function(event) {
         event.preventDefault();
 
         $.ajax({
             url: queryURL,
             method: 'GET'
-        }).done(function(response) {
+        }).done(function(data) {
+            console.log(data);
 
-        var result = response.data; 
+            for (var i = 0; i < data[i].length; i++) {
+        
+                    var gifDiv = $("<div class='gif'>");
 
-        for (var i = 0; i < result.length; i++) {
-        var imageUrl = result[i].images;
-        var img = $("<img>");
-        img.attr("src", imageUrl);
-        img.addClass("gif");
-        img.attr("data-still", result[i].images.fixed_height_still.url);
-        img.attr("data-animate", result[i].images.fixed_height.url);
-        img.attr("data-state", "still");
+                    var rating = data[i].rating;
+                    var r = $("<p>").text("Rating: " + rating);
+                    gifDiv.append(r);
 
-        var caption = result[i].caption;
-        var c = $("<p>").text("Caption: " + caption);
-        img.append(c);
+                    var img = $("<img>");
 
-        $("#engineer-view").append(img); // APPENDS buttons at TOP in engineering view DIV
-            }
+                    img.attr("src", data[i].images.fixed_height_still.url);
+                    img.attr(data[i].images.fixed_height.url);
+                    //img.attr(data[i].type);
+                    img.attr("data-state");
+
+                    gifDiv.append(r);
+                    gifDiv.append(img);
+                    $("#engineering-view").prepend(gifDiv); // APPENDS buttons at TOP in engineering view DIV
+                }
         });
     });
 
-    $("img").on("click", function() {
+    $(".gif").on("click", function() {
 
       var state = $(this).attr("data-state");
-
       if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
       } else {
         $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
       }
-    });
+});
 
     $("#user-submit").on("click", function(event) {
         event.preventDefault();
@@ -65,9 +68,8 @@ function generateButtons() {
         var userInput = $("#engineering-term").val().trim();
         topics.push(userInput);
         generateButtons();
+});
 
-    });
-}
 $(document).on("click");
 
 generateButtons();
